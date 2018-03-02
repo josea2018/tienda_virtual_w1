6cb84cb96c45844723f5d2e24f5fb4ca02025148
josea2018 <josea201812@gmail.com>
2018-03-01 20:01:37 -0600

    seguridad para ingresar a home

--- home/index.php
+++ home/index.php
@@ -1,7 +1,7 @@
 <?php
   
   $titulo = 'Home';
-  //include '../seguridad/verificar_session.php';
+  include '../seguridad/verificar_session.php';
   include '../shared/menu.php';
   include '../shared/header.php';
   include '../shared/footer.php';
